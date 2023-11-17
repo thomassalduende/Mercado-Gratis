bb49ee4af7dd811843d8dc6d617fd3ffe72f3b75
thomassalduende <thomi44sal@gmail.com>
2023-11-17 11:47:44 -0300

    actualizado

--- src/app/items/[id]/page.tsx
+++ src/app/items/[id]/page.tsx
@@ -6,7 +6,7 @@ export default async function ItemPage({ params: { id } }: { params: { id: strin
     const item = await api.item.fetch(id);
 
     return (
-        <section className="grid gap-2">
+        <section className="grid gap-2" key={item.id}>
             <img src={item.thumbnail} alt={item.title} />
             <p className="text-xl font-bold">
                 {Number(item.price).toLocaleString("es-AR", { style: 'currency', currency: item.currency_id })}
